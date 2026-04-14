caf3dae5a95c6cfe8b9e9d4d1ef26db804919205
Rudi <maboitemail.romeu@gmail.com>
2026-04-14 15:27:10 +0200

    chore: mise à jour cv-data.js via admin

--- cv-data.js
+++ cv-data.js
@@ -1,61 +1,100 @@
 const CV_DEFAULT_DATA = {
-  personal: {
-    name: "Rudi Romeu",
-    title: "Développeur Web",
-    address: "100 bd Masséna, 75013\nParis",
-    phone: "+33 642556699",
-    email: "rudi@gmail.com",
-    website: "www.rudi-romeu.fr"
+  "personal": {
+    "name": "Rudi Romeu",
+    "title": "Développeur FullStack/DevOpps",
+    "phone": "+33 642556699",
+    "email": "rudi.romeu@gmail.com",
+    "website": "www.rudiromeu.xyz",
+    "address": "1 bd Masséna, 75013 Paris",
+    "avatarUrl": ""
   },
-  about: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis illo fugit officiis distinctio culpa officia totam atque exercitationem inventore repudiandae?",
-  languages: [
-    { name: "Français", level: 5 },
-    { name: "Anglais", level: 3 },
-    { name: "Espagnol", level: 2 }
-  ],
-  skills: [
-    { name: "HTML 5", percent: 90 },
-    { name: "CSS 3", percent: 80 },
-    { name: "JS", percent: 0 }
-  ],
-  social: [
-    { name: "Linkedin", icon: "fab fa-linkedin", handle: "Rudi@linkedin" },
-    { name: "Twitter", icon: "fab fa-twitter-square", handle: "Rudi@twitter" },
-    { name: "Facebook", icon: "fab fa-facebook-square", handle: "Rudi@facebook" }
-  ],
-  experiences: [
+  "about": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis illo fugit officiis distinctio culpa officia totam atque exercitationem inventore repudiandae?",
+  "skills": [
     {
-      dates: "2020 - Aujourd'hui",
-      title: "Lorem ipsum dolor sit amet.",
-      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, voluptatibus!"
+      "name": "HTML 5",
+      "percent": 90
     },
     {
-      dates: "2017 - 2020",
-      title: "Lorem ipsum dolor sit amet.",
-      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, voluptatibus!"
+      "name": "CSS 3",
+      "percent": 80
     },
     {
-      dates: "2015 - 2017",
-      title: "Lorem ipsum dolor sit amet.",
-      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, voluptatibus!"
+      "name": "JS",
+      "percent": 0
     }
   ],
-  education: [
+  "languages": [
     {
-      dates: "2010 - 2013",
-      title: "Lorem ipsum dolor sit amet.",
-      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, voluptatibus!"
+      "name": "Français",
+      "level": 5
     },
     {
-      dates: "2007 - 2010",
-      title: "Lorem ipsum dolor sit amet.",
-      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, voluptatibus!"
+      "name": "Anglais",
+      "level": 3
+    },
+    {
+      "name": "Espagnol",
+      "level": 2
     }
   ],
-  hobbies: [
-    { icon: "fas fa-film" },
-    { icon: "fas fa-basketball-ball" },
-    { icon: "fas fa-motorcycle" },
-    { icon: "fas fa-plane" }
+  "social": [
+    {
+      "name": "Linkedin",
+      "handle": "Rudi@linkedin",
+      "icon": "fab fa-linkedin"
+    },
+    {
+      "name": "Twitter",
+      "handle": "Rudi@twitter",
+      "icon": "fab fa-twitter-square"
+    },
+    {
+      "name": "Facebook",
+      "handle": "Rudi@facebook",
+      "icon": "fab fa-facebook-square"
+    }
+  ],
+  "experiences": [
+    {
+      "dates": "2020 - Aujourd'hui",
+      "title": "Lorem ipsum dolor sit amet.",
+      "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, voluptatibus!"
+    },
+    {
+      "dates": "2017 - 2020",
+      "title": "Lorem ipsum dolor sit amet.",
+      "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, voluptatibus!"
+    },
+    {
+      "dates": "2015 - 2017",
+      "title": "Lorem ipsum dolor sit amet.",
+      "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, voluptatibus!"
+    }
+  ],
+  "education": [
+    {
+      "dates": "2010 - 2013",
+      "title": "Lorem ipsum dolor sit amet.",
+      "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, voluptatibus!"
+    },
+    {
+      "dates": "2007 - 2010",
+      "title": "Lorem ipsum dolor sit amet.",
+      "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, voluptatibus!"
+    }
+  ],
+  "hobbies": [
+    {
+      "icon": "fas fa-film"
+    },
+    {
+      "icon": "fas fa-basketball-ball"
+    },
+    {
+      "icon": "fas fa-motorcycle"
+    },
+    {
+      "icon": "fas fa-plane"
+    }
   ]
 };
